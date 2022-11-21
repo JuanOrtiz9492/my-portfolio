7771d5e89b354d141f73121084edce0cd74788ff
Juan Ortiz <ortizjuandavid@gmail.com>
2022-11-20 22:39:31 -0500

    updated project ecomerce

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,11 +3,16 @@ const cards = [
 		id: 1,
 		projectName: 'Fire Marketing',
 		sumary: 'e comerce related to alcohol industry',
-		description:
-			'This is an active e comerce developed in react, express nginx and postgres. was build for a b2c for alcohol related products',
+		description: `This e-commerce was built with the goal to be responsive fast loading times, this last part was achieved by implementing the correct size for images and compressing these files. Also, I avoided the use of a UI-library to keep the css as small as possible.
+
+			The state is managed by redux, the styles were applyed with the help of styledcomponents.
+			
+			In the service side, I implemented a NodeJS server along with express and Knex as an ORM to communicate with the database,
+			
+			The project that you can visit right now is running on a Linux sever inside of a docker container`,
 		img: './fire-marketing.jpg',
 		isVisitable: true,
-		url: 'https://fire-marketing.com/',
+		url: 'http://juanortizdev.com/ecomerce',
 	},
 	{
 		id: 2,
